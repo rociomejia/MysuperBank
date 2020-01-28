426fc64454f3dd4af4851517a61ca8e22a7fb29a
Rocio <chiocjava@yahoo.es>
2020-01-28 10:51:34 -0700

    'first'

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MysuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var account = new BankAccount("rose", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
